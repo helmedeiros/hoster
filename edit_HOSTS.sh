184a4155bee08e814aad1274a37224f9012402e7
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2014-02-05 19:33:46 -0200

    changed built ins to their folder

--- edit_HOSTS.sh
+++ edit_HOSTS.sh
@@ -10,8 +10,8 @@
 #     # Edit an specific environment file (localhost -l; development -d; homologation -h; production -p)
 #      ./edit_HOST.sh -l 
 source $(dirname $0)/commands.sh
-source $(dirname $0)/handle_options.sh
-source $(dirname $0)/paths.sh
+source $(dirname $0)/builtin/handle_options.sh
+source $(dirname $0)/builtin/paths.sh
 
 FILE="Hosts";
 FILE_PATH="/private/etc/";
@@ -19,7 +19,7 @@ network="Wi-Fi";
 
 DEFAULT_IDE="sublime";
 
-
+  
 handle_options $@;
 set_path;
 
