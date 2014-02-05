dd77c916aba5d48370ebdd8b92416e22e3b6f4c5
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2014-02-05 19:23:50 -0200

    extracted common options handler method

--- change_HOSTS.sh
+++ change_HOSTS.sh
@@ -9,70 +9,77 @@
 #
 #     # Edit an specific environment file (localhost -l; development -d; homologation -h; production -p)
 #      ./change_HOST.sh -l 
-source $(dirname $0)/commands.sh
+source $(dirname $0)/commands.sh    
 
 FILE="Hosts";
 FILE_PATH="/private/etc/";
 network="Wi-Fi";
 
-# Parse user options
-#
-while getopts "ldhpn:" opt; do
-  case $opt in
+function handle_options(){
+  # Parse user options
+  #
+  while getopts "ldhpn:" opt; do
+    case $opt in
 
-    # Set localhost's host file
-    l)
-        echo "-choose LOCAL!" >&2
-        FILE="hosts.lch";
+      # Set localhost's host file
+      l)
+          echo "-choose LOCAL!" >&2
+          FILE="hosts.lch";
+        ;;
+
+      # Set development's host file
+      d)
+          echo "-choose DEVELOPMENT!" >&2
+          FILE="hosts.dev";
+        ;;
+
+      # Set homolagtion's host file
+      h)
+          echo "-choose HOMOLOGATION!" >&2
+          FILE="hosts.hml";
+        ;;
+
+      # Set production's host file
+      p)
+          echo "-choose PRODUCTION!" >&2
+          FILE="hosts.prd";
+        ;;
+
+      n)
+        if [[ "$OPTARG" =~ ^[.*] ; then
+          network=${OPTARG}   
+          echo "$network";
+          ROUTER=$(networksetup -getinfo $network | grep '^Router:' | awk '{print $2}')
+        else
+          die "Invalid parameter for network: ${OPTARG}."
+        fi
+        ;;
+
+
+      \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
       ;;
-
-    # Set development's host file
-    d)
-        echo "-choose DEVELOPMENT!" >&2
-        FILE="hosts.dev";
+      :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
       ;;
+    esac
+  done
+}
 
-    # Set homolagtion's host file
-    h)
-        echo "-choose HOMOLOGATION!" >&2
-        FILE="hosts.hml";
-      ;;
+function define_path(){
+  echo $ROUTER;
 
-    # Set production's host file
-    p)
-        echo "-choose PRODUCTION!" >&2
-        FILE="hosts.prd";
-      ;;
-
-    n)
-      if [[ "$OPTARG" =~ ^[.*] ; then
-        network=${OPTARG}   
-        echo "$network";
-        ROUTER=$(networksetup -getinfo $network | grep '^Router:' | awk '{print $2}')
-      else
-        die "Invalid parameter for network: ${OPTARG}."
-      fi
-      ;;
-
-
-    \?)
-    echo "Invalid option: -$OPTARG" >&2
-    exit 1
-    ;;
-    :)
-    echo "Option -$OPTARG requires an argument." >&2
-    exit 1
-    ;;
-  esac
-done
-
-echo $ROUTER;
-
-case $ROUTER in
-  "172.20.92.1")  FILE_PATH="~/Dropbox/JOBS/RBS/Hosts/";
-          echo "CONFIGURING RBS";
+  case $ROUTER in
+    "172.20.92.1")  FILE_PATH="~/Dropbox/JOBS/RBS/Hosts/";
+            echo "CONFIGURING RBS";
           ;;
-esac
+  esac
+}
+
+handle_options $@;
+define_path;
 
 run_cmd "mv $FILE_PATH/Hosts $FILE_PATH/tmp"
 run_cmd "cp $FILE_PATH$FILE $FILE_PATH/Hosts"
--- edit_HOSTS.sh
+++ edit_HOSTS.sh
@@ -10,69 +10,26 @@
 #     # Edit an specific environment file (localhost -l; development -d; homologation -h; production -p)
 #      ./edit_HOST.sh -l 
 source $(dirname $0)/commands.sh
+source $(dirname $0)/handle_options.sh
 
 FILE="Hosts";
 FILE_PATH="/private/etc/";
 network="Wi-Fi";
 
-# Parse user options
-#
-while getopts "ldhpn:" opt; do
-  case $opt in
-
-    # Set localhost's host file
-    l)
-        echo "-choose LOCAL!" >&2
-        FILE="hosts.lch";
-      ;;
-
-    # Set development's host file
-    d)
-        echo "-choose DEVELOPMENT!" >&2
-        FILE="hosts.dev";
-      ;;
-
-    # Set homolagtion's host file
-    h)
-        echo "-choose HOMOLOGATION!" >&2
-        FILE="hosts.hml";
-      ;;
-
-    # Set production's host file
-    p)  
-        echo "-choose PRODUCTION!" >&2
-        FILE="hosts.prd";
-      ;;
-
-    n)
-      if [ -n "$1" ]; then    
-        network=${OPTARG}   
-        ROUTER=$(networksetup -getinfo $network | grep '^Router:' | awk '{print $2}')
-      else
-        die "Invalid parameter for network: ${OPTARG}."
-      fi
-      ;;
-
-
-    \?)
-    echo "Invalid option: -$OPTARG" >&2
-    exit 1
-    ;;
-    :)
-    echo "Option -$OPTARG requires an argument." >&2
-    exit 1    
-    ;;
-  esac
-done
-
 DEFAULT_IDE="sublime";
 
-echo $ROUTER;
 
-case $ROUTER in
-  "172.20.92.1")  FILE_PATH="~/Dropbox/JOBS/RBS/Hosts/";
-          echo "CONFIGURING RBS";
-          ;;
-esac
+function set_path(){
+    echo $ROUTER;
+
+    case $ROUTER in
+      "172.20.92.1")  FILE_PATH="~/Dropbox/JOBS/RBS/Hosts/";
+              echo "CONFIGURING RBS";
+              ;;
+    esac
+}
+
+handle_options $@;
+set_path;
 
 run_cmd "$DEFAULT_IDE $FILE_PATH$FILE"
