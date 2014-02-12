7520dbc00858aabe1980283dbe0b35bf719b93e7
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2014-02-12 10:23:06 -0200

    Changed usage description

--- hoster.sh
+++ hoster.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 #
-# edit_HOST.sh - An host file editor;
+# hoster.sh - An host file controler;
 #
 # Usage:
 #
 #     # Edit the default host file
-#      ./edit_HOST.sh
+#      hoster.sh -e
 #
 #     # Edit an specific environment file (localhost -l; development -d; homologation -h; production -p)
-#      ./edit_HOST.sh -l 
+#      hoster -p -e
+
 source $(dirname $0)/commands.sh
 source $(dirname $0)/builtin/handle_options.sh
 source $(dirname $0)/builtin/defaults.sh
