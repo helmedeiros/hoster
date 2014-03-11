a1a163dbfafa4dd8812c7474fda609154dc10018
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2014-03-11 00:39:38 -0300

    add new edition functions to set envirnment definitions and edit file

--- builtin/edition.sh
+++ builtin/edition.sh
@@ -7,3 +7,13 @@ function apply_host(){
 	run_cmd "sudo cp $HOST_PATH/Hosts $HOST_PATH/tmp"
 	run_cmd "sudo cp $FILE_PATH$FILE $HOST_PATH/Hosts"
 }
+
+function edit_host(){
+	handle_env_options $2;
+}
+
+function hosts_edit(){
+	cmd_set_environment $ENVIRONMENT;
+	cmd_top_level;
+	run_cmd "$DEFAULT_IDE $TOP_LEVEL_FOLDER/$FILE" "silent";
+}
