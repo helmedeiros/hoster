a721832075a3ea19513873eb03cd4675ca5b87bb
Iuri Andreazza <iuri.andreazza@gmail.com>
2014-03-11 23:38:51 -0300

    - Adjusting the project structure to clear the root path - Removed the BIN folder from VCS - Incremented the version

--- Library/Build/build.sh
+++ Library/Build/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 rm /Library/Caches/Homebrew/hoster-0.1.tar.gz 
 brew remove hoster
-brew install --HEAD --verbose --debug hoster
+#brew install --HEAD --verbose --debug hoster
+brew install --HEAD hoster
 brew test hoster
 
 
