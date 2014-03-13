87b5135c24c17cfcce62d7a43e7f3cff69526bb2
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2014-03-12 22:59:55 -0300

    change help to new metofo.

--- help.sh
+++ help.sh
@@ -12,12 +12,8 @@ function list_commands(){
 
 function list_common_cmds_help(){
 	echo "The most commonly used `basename $0` commands are";
-	echo "    dev 	Define the DEVELOPMENT environment hosts to be used.";
+	echo "    add 	Add a new HOST to current repository into a specific environment.";
 	echo "    edit	Open the host file defined to be used.";
-	echo "    hlg		Define the HOMOLOGATION environment hosts to be used.";
 	echo "    init	Create an empty host repository in the current folder.";
-	echo "    lcl		Define the LOCAL environment hosts to be used.";
-	echo "    net		Check the network # [Ethernet | Wi-Fi | VPN] to chose specific files based on what will you do there.";
-	echo "    prod	Define the PRODUCTION environment hosts to be used.";
-	echo "    set		Set the host file defined to be used.";
+	echo "    list	List all hosts for a specific project.";
 }
