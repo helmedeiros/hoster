ff51d8514562bd0c6925df1ffcbdf8fffaa2b0e7
helmedeiros <helio.cabralmedeiros@gmail.com>
2024-05-07 10:00:00 -0300

    Quote variables and expand tilde via $HOME in paths.sh
    Quotes $ROUTER in the echo and case statement and replaces the
    literal "~" in the assignment with $HOME (tilde does not expand
    inside double quotes, SC2088).

--- builtin/paths.sh
+++ builtin/paths.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 #
 function set_path(){
-    echo $ROUTER;
+    echo "$ROUTER";
 
-    case $ROUTER in
-      "172.20.92.1")  FILE_PATH="~/Dropbox/JOBS/RBS/Hosts/";
+    case "$ROUTER" in
+      "172.20.92.1")  FILE_PATH="$HOME/Dropbox/JOBS/RBS/Hosts/";
               echo "CONFIGURING RBS";
               echo "$FILE_PATH";
               ;;
