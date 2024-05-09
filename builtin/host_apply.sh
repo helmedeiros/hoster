37eab014e3532f20926b5b9b2727f50e3b404c46
helmedeiros <helio.cabralmedeiros@gmail.com>
2024-05-09 10:00:00 -0300

    Quote scalars in apply_host and hosts_apply
    Quotes $2, ENVIRONMENT and HOST_FILE_TO_APPEND. append_host and the
    sudo/sed pipeline are deeper concerns covered in follow-up commits.

--- builtin/host_apply.sh
+++ builtin/host_apply.sh
@@ -2,14 +2,14 @@
 
 
 function apply_host(){
-	handle_env_options $2;
+	handle_env_options "$2";
 }
 
-function hosts_apply(){	
-	cmd_set_environment $ENVIRONMENT;
-	
+function hosts_apply(){
+	cmd_set_environment "$ENVIRONMENT";
+
 	HOST_FILE_TO_APPEND="$TOP_LEVEL_FOLDER/$FILE";
-	cmd_append_hosts $HOST_FILE_TO_APPEND;
+	cmd_append_hosts "$HOST_FILE_TO_APPEND";
 }
 
 function append_host(){
