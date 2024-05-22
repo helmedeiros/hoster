97beddcd67c07bcc369c11586569911e1c139819
helmedeiros <helio.cabralmedeiros@gmail.com>
2024-05-22 10:00:00 -0300

    Modernize Homebrew formula
    - Drop the boilerplate Formula-Cookbook comments
    - Add desc and license fields (now required by brew audit)
    - Switch the url back to the canonical helmedeiros fork and pin
      sha256 instead of the deprecated sha1
    - Replace "depends_on :python" with python3 in the install step
      (the symbol-style python dep was removed from Homebrew years ago)
    - Use bin/"hoster" in tests so brew picks up the installed binary

--- Library/Formula/hoster.rb
+++ Library/Formula/hoster.rb
@@ -1,43 +1,23 @@
-require "formula"
-
-# Documentation: https://github.com/Homebrew/homebrew/wiki/Formula-Cookbook
-#                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Hoster < Formula
+  desc "Project-scoped /etc/hosts management for local, dev, hlg and prod environments"
   homepage "https://github.com/helmedeiros/hoster"
-  head "https://github.com/iuriandreazza/hoster.git" 
-  url "https://github.com/iuriandreazza/hoster/releases/download/1.7.1/hoster-1.7.1-IURI-as.tar.gz"
-  sha1 "96252f3506ee953bd49a2cb281977155e593ab94"
+  url "https://github.com/helmedeiros/hoster/releases/download/1.7.2-IURI/hoster-1.7.2-IURI-as.tar.gz"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  license "MIT"
+  head "https://github.com/helmedeiros/hoster.git", branch: "master"
 
-  depends_on :python
-  depends_on 'tree'
+  depends_on "tree"
 
   def install
-    ENV.deparallelize
-    #Reject py, and copy all tgz folders
-    prefix.install Dir['*'].reject{|f|f['*.py'] }
-    system "python", (prefix + "Library/Setup/setup.py"), prefix, version
+    prefix.install Dir["*"].reject { |f| f["*.py"] }
+    system "python3", prefix/"Library/Setup/setup.py", prefix, version
     man.mkpath
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test hoster`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    print "-> Testing Hoster Installation"
-    print "."
-    system "hoster", "--version"
-    
-    print "-> Testing Creating HOSTER Strucutre"
-    print "."
-    system "hoster", "init"
-    system "hoster", "add", "127.0.0.1", "www.test.com", "--dev"
-    system "hoster", "list"
+    system bin/"hoster", "--version"
+    system bin/"hoster", "init"
+    system bin/"hoster", "add", "127.0.0.1", "www.test.com", "--dev"
+    system bin/"hoster", "list"
   end
 end
